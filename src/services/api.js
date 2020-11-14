e868cbf2fe2f25ca3600ff71d55fde0778536f23
Vinicius Benedito <beneditoloura@gmail.com>
2020-11-14 11:45:26 -0300

    Requisões para login, cadastro e verificação de token feitas

--- src/services/api.js
+++ src/services/api.js
@@ -1,15 +1,42 @@
-const BASE_API = '';
+const BASE_API = 'https://api.b7web.com.br/devbarber/api';
 
 export default {
-    ckeckToken: async () => {
-
+    ckeckToken: async (token) => {
+        const req = await fetch(`${BASE_API}/auth/refresh`, {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ token })
+        });
+        const json = await req.json();
+        return json;
     },
 
-    signIn: async () => {
-
+    signIn: async (email, password) => {
+        const req = await fetch(`${BASE_API}/auth/login`, {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ email, password })
+        });
+        const json = await req.json();
+        return json;
     },
 
-    signUp: async () => {
-
+    signUp: async (name, email, password) => {
+        const req = await fetch(`${BASE_API}/user`, {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ name, email, password })
+        });
+        const json = await req.json();
+        return json;
     }
 };
