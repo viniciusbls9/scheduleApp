f7e67d2d6eafd4c8aa1e273d0f2425af63e16582
Vinicius Benedito <beneditoloura@gmail.com>
2020-11-17 07:37:17 -0300

    Tela de preload configurada

--- src/screens/Preload/index.js
+++ src/screens/Preload/index.js
@@ -1,20 +1,39 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useContext } from 'react';
 import AsyncStorage from '@react-native-community/async-storage';
 import { useNavigation } from '@react-navigation/native';
+import { UserContext } from '../../contexts/UserContext';
 
+import api from '../../services/api';
 import BarberLogo from '../../assets/barber.png';
 import { Container, BarberImg, LoadingIcon } from './styles';
 
 
 export default () => {
-
+    const { dispatch: UserDispatch } = useContext(UserContext);
     const navigation = useNavigation();
 
     useEffect(() => {
-        const checkToken = async() => {
+        const checkToken = async () => {
             const token = await AsyncStorage.getItem('token');
-            if(token !== null) {
-                // validar o token
+            if (token) {
+                let res = await api.ckeckToken(token);
+                if (res.token) {
+                    await AsyncStorage.setItem('token', res.token);
+
+                    UserDispatch({
+                        type: 'setAvatar',
+                        payload: {
+                            avatar: res.data.avatar
+                        }
+                    });
+
+                    navigation.reset({
+                        routes: [{ name: 'MainTab' }]
+                    });
+                } else {
+                    navigation.navigate('SignIn');
+                }
+
             } else {
                 navigation.navigate('SignIn');
             }
